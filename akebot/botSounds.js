ddbdfd6804c16c7b92bbc4b55107a414e8708c8c
Mesmaroth <mesmaroth666@live.com>
2016-04-18 14:11:04 -0700

    Removed useless comment

--- akebot/botSounds.js
+++ akebot/botSounds.js
@@ -10,14 +10,14 @@ function searchSong(message, soundsList){
     return null;
 }
 
-module.exports.playSound = function(bot, channelID, message) {             // Plays sound effects on the first voice channel listed.
+module.exports.playSound = function(bot, channelID, message) {
 	var soundsList = fs.readdirSync('./sounds');
 	var songNum = searchSong(message, soundsList);
 	if(songNum === null) return;
 	var voiceChannelID = "";
 	var file = 'sounds/'+soundsList[songNum];
 	
-    for(var i in bot.servers[bot.serverFromChannel(channelID)].channels){
+    for(var i in bot.servers[bot.serverFromChannel(channelID)].channels){       // Get first voice channel
         if(bot.servers[bot.serverFromChannel(channelID)].channels[i].type === "voice" && bot.servers[bot.serverFromChannel(channelID)].channels[i].position === 0){
             voiceChannelID = bot.servers[bot.serverFromChannel(channelID)].channels[i].id;
             break;
@@ -27,9 +27,9 @@ module.exports.playSound = function(bot, channelID, message) {             // Pl
     bot.joinVoiceChannel(voiceChannelID, function(){
         bot.getAudioContext({channel: voiceChannelID, stereo: true}, function (stream){
         	stream.playAudioFile(file);
-        	stream.once('fileEnd', function(){
-        		bot.leaveVoiceChannel(voiceChannelID);
-        	})
+            stream.once('fileEnd', function(){
+                bot.leaveVoiceChannel(voiceChannelID);
+            });
         });
     });
 }
