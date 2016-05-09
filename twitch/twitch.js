91ea34be2555bb701b2045127a7e1d29c4205ff2
Mesmaroth <mesmaroth666@live.com>
2016-05-09 13:30:28 -0700

    Updated twitch for main

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -1,50 +1,10 @@
 var twitchClient = require('node-twitchtv');
-var account = require('./account.json')
-var client = new twitchClient(account);
-var fs = require('fs');
+var client = new twitchClient('{"client_id" : "CLIENT_ID"}'); // Not required leave as is, it should still work
 
 
-module.exports.checkTwitchUser = function (searchUser, channelID, bot){
+module.exports.checkStream = function (searchUser, callback){     // Checks to see if the stream is live
     client.streams({channel: searchUser}, function (error, user) {
-        if(error) throw error;
-        if(user.stream !== null){
-            bot.sendMessage({
-                to: channelID,
-                message: "**Twitch**\n**User**: "+ user.stream.channel.name + "\n**Status**: `Online`\n**Game**: "+ user.stream.game+"\n**Url**: "+user.stream.channel.url
-            });
-        }
-        else if(user.stream === null) {
-            bot.sendMessage({
-                to: channelID,
-                message: "**Twitch**\n**User**: "+ searchUser + "\n**Status**: `Offline`"
-            });
-        }
+        if(error) return console.log(error);
+        callback(user);
     });
-}
-
-function getStreamers (username, callback) {
-    client.streams({channel: username}, function (error, user){
-        if(error) throw error;
-        if(user.stream != null){
-            callback(user.stream.channel.name, user.stream.game, user.stream.channel.url);
-        }
-    })
-}
-
-module.exports.searchTwitch = function (bot, channelID){
-    var streamers = fs.readFileSync('./twitch/twitchUserList.txt', 'utf8').split('\n');
-    bot.sendMessage({
-        to: channelID,
-        message: "**Twitch Status - BETA**"
-    });
-    var tick = 0;
-    for(var i = 0; i < streamers.length; i++){
-        getStreamers(streamers[i], function(tName, tGame, tUrl){
-            bot.sendMessage({
-                to: channelID,
-                message: "**Twitch**\n**User**: "+ tName + "\n**Status**: `Online`\n**Game**: "+ tGame+"\n**Url**: <" + tUrl+">"
-            })
-        });
-    }
-    
 }
