e7d331fb7903738c298ea7df82f55fb1c8882de1
Mesmaroth <mesmaroth666@live.com>
2016-05-12 18:05:16 -0700

    Simplifed live check for all twitch commands, using callbacks

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -3,8 +3,13 @@ var client = new twitchClient('{"client_id" : "CLIENT_ID"}'); // Not required le
 
 
 module.exports.checkStream = function (searchUser, callback){     // Checks to see if the stream is live
-    client.streams({channel: searchUser}, function (error, user) {
+    client.streams({channel: searchUser}, function (error, userStream) {
         if(error) return console.log(error);
-        callback(user);
+        if(userStream.stream !== null){
+            callback(true, userStream.stream.channel.name, userStream.stream.game, userStream.stream.channel.url);
+        }
+        else if(userStream.stream === null){
+            callback(false);
+        }
     });
 }
