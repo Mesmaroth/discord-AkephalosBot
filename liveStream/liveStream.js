edeb15e588a73aa1f88ceac91e62771037c5bddd
Mesmaroth <mesmaroth666@live.com>
2016-05-21 22:56:14 -0700

    Hitbox and Twitch separated.

--- liveStream/liveStream.js
+++ liveStream/liveStream.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 // Check if Hitbox user is live
-function getHitBoxStream(searchUser, callback){
+function getHitBoxStatus(searchUser, callback){
 	request('https://api.hitbox.tv/user/' + searchUser, function(error, response, body){
 		body = JSON.parse(body);
 		if(!error && response.statusCode == 200){
@@ -15,35 +15,34 @@ function getHitBoxStream(searchUser, callback){
 	});
 }
 
-module.exports.getStreamStatus = function (searchUser, callback){     // Checks to see if the stream is live
+module.exports.getTwitchStream = function(searchUser, callback){     // Checks to see if the stream is live
     request('https://api.twitch.tv/kraken/streams/'+searchUser, function(error, response, body){
 		body = JSON.parse(body);
 		if(!error && response.statusCode == 200){
 			if(body.stream != null){
-				callback(true, "Twitch", body.stream.channel.name, body.stream.game, body.stream.channel.url);
+				callback(true, body.stream.channel.name, body.stream.game, body.stream.channel.url);
 				return;
 			}
-			else{
-				callback(false)
-			};
 		}
-	});
+	});   
 
-    getHitBoxStream(searchUser, function(status){
+    callback(false);
+}
+
+module.exports.getHitBoxStream = function(searchUser, callback){
+
+	getHitBoxStatus(searchUser, function(status){
     	if(status){
-    		// Get Live Info
+    		// If Live then get Live Info
     		request('https://api.hitbox.tv/media/live/' + searchUser, function(error, response, body){
     			body = JSON.parse(body);
 				if(!error && response.statusCode == 200){
-					callback(true, "HitBox", body.livestream[0].media_user_name, body.livestream[0].category_name, "http://www.hitbox.tv/"+searchUser);
+					callback(true, body.livestream[0].media_user_name, body.livestream[0].category_name, "http://www.hitbox.tv/"+searchUser);
 					return;
-				}
-				else{
-					callback(false);
 				}
 			});
     	}	    	
     });
 
-    
+    callback(false);
 }
