a6a4e165a1cd81cf024714f61b96524be9bba6bc
Mesmaroth <mesmaroth666@live.com>
2016-06-04 17:39:14 -0700

    Code Revised

--- akebot/botSounds.js
+++ akebot/botSounds.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
 
 function searchSong(message, soundsList){
-    var songName = message.slice(1);
+    var soundName = message.slice(1);
     for(var i = 0; i < soundsList.length; i++){
-        var soundListName = soundsList[i].split('.');   // Splitting the name and extension
-        soundListName = soundListName[0];
-        if(soundListName === songName) return i;
+        var soundListName = soundsList[i].split('.')[0];
+        if(soundListName === soundName) return i;
     }
     return null;
 }
@@ -17,12 +16,14 @@ module.exports.playSound = function(bot, channelID, message) {
 	var voiceChannelID = "";
 	var file = 'sounds/'+soundsList[songNum];
 	
-    for(var i in bot.servers[bot.serverFromChannel(channelID)].channels){       // Get first voice channel
-        if(bot.servers[bot.serverFromChannel(channelID)].channels[i].type === "voice" && bot.servers[bot.serverFromChannel(channelID)].channels[i].position === 0){
-            voiceChannelID = bot.servers[bot.serverFromChannel(channelID)].channels[i].id;
+    var serverChannels = bot.servers[bot.serverFromChannel(channelID)].channels;
+    for(var i in serverChannels){       // Get first voice channel
+        if(serverChannels[i].type === "voice" && serverChannels[i].position === 0){
+            voiceChannelID = serverChannels[i].id;
             break;
         }
     }
+
     // play
     bot.joinVoiceChannel(voiceChannelID, function(){
         bot.getAudioContext({channel: voiceChannelID, stereo: true}, function (stream){
