388f6d55cad4cefa47d5165b4d6849dbd183ec8e
Mesmaroth <mesmaroth666@live.com>
2016-07-09 19:11:38 -0700

    Updated login file

--- akebot/botLogin.js
+++ akebot/botLogin.js
@@ -1,5 +1,13 @@
+// All API crendetials will be here
+// The bot only requires a token if you have a bot account
+// If you don't want to use a bot account, you can instead use email and password
+
+// *If cleverbot crendentials aren't correct, the bot will crash
+
 module.exports = {
+	token: "{Token}",
 	email: "{EMAIL}",
 	password: "{PASSWORD}",
-	token: "{TOKEN}"
+	cleverBot_API_User: "{API_USER}",
+	cleverBot_API_Key: "{API_KEY}"
 }
