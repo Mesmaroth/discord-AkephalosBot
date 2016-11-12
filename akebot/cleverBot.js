3912a9dd3163f42bcfb9539f841da978f79ee7fa
mesmaroth <mesmaroth666@live.com>
2016-11-11 22:29:26 -0800

    Fixed credentials and request

--- akebot/cleverBot.js
+++ akebot/cleverBot.js
@@ -1,11 +1,11 @@
-var cleverbot = require('cleverbot.io');
-var botLogin = require("./botLogin.js");
-var cBot = new cleverbot(botLogin.cleverBot_API_User, botLogin.cleverBot_API_Key, "AkeSession");
-
-module.exports = function (request, callback){	
-	cBot.create(function (error, session){
+module.exports = function (input, callback){
+	var cleverbot = require('cleverbot.io');
+	var botLogin = require("./botLogin.js");
+	var cBot = new cleverbot(botLogin.cleverBot_API_User, botLogin.cleverBot_API_Key);
+	
+	cBot.create(function (error){
 	    if(error) return callback(error);
-	    cBot.ask(request, function (error, response){
+	    cBot.ask(input, function (error, response){
 		    if(error) return callback(error);
 		    return callback(null, response);
 	    });
