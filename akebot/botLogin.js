10a08b6f221980d2d725fc56d056ae800edfeddb
mesmaroth <mesmaroth666@live.com>
2016-11-11 21:22:38 -0800

    Comments and twitch client ID

--- akebot/botLogin.js
+++ akebot/botLogin.js
@@ -1,13 +1,14 @@
 // All API crendetials will be here
 // The bot only requires a token if you have a bot account
+// You can get a bot token at https://discordapp.com/developers/applications/me
 // If you don't want to use a bot account, you can instead use email and password
-
-// *If cleverbot crendentials aren't correct, the bot will crash
-
+// Cleverbot keys can be found at https://cleverbot.io/
+// Twitch credentals can be found at https://dev.twitch.tv/
 module.exports = {
-	token: "{Token}",
-	email: "{EMAIL}",
-	password: "{PASSWORD}",
-	cleverBot_API_User: "{API_USER}",
-	cleverBot_API_Key: "{API_KEY}"
+	token: "TOKEN",
+	email: "EMAIL",
+	password: "PASSWORD",
+	cleverBot_API_User: "CLEVERBOT API USER KEY",
+	cleverBot_API_Key: "CLEVERBOT API KEY",
+	twitch_Client_ID: "TWITCH CLIENT ID"
 }
