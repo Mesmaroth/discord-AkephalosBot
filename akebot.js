37ea7499f3100423aa6a1f24ad70a3f2c0ca2aca
Robert <Mesmaroth666@live.com>
2017-04-15 12:16:47 -0700

    Added error logging and response, added console info

--- akebot.js
+++ akebot.js
@@ -4,11 +4,8 @@ const bot = new Discord.Client();
 bot.login(botLogin.token);
 
 const adminRole = "admin";
-const defaultStatus = botVersion + " | " + CMDINT + "help";
 
 var botVersion = "?#";
-var CMDINT = "?";
-
 try{
 	botVersion = require('./package.json').version;
 } catch(error){
@@ -18,6 +15,10 @@ try{
 		console.log("----------------------");
 	}
 }
+var CMDINT = "?";
+var defaultStatus = botVersion + " | " + CMDINT + "help";
+
+
 
 // Checks if the message is a command input from the user
 function isCommand(message, command){
@@ -31,7 +32,7 @@ function isCommand(message, command){
 }
 
 // Checks for a specific role the user is in to run admin commands
-function isDev(message){
+function isAdmin(message){
 	var roles = message.member.roles.array();
 	for(var role = 0; role < roles.length; role++){
 		if(roles[role].name.toLowerCase() === adminRole)			
@@ -46,26 +47,71 @@ function setGame(game){
 	bot.user.setGame(game);
 }
 
+function getChannelByName(guild, channelName){
+	return guild.channels.filterArray( channel => {
+		if(channel.name === channelName)
+			return channel;
+	})[0];
+}
+
+function botLog(message){
+	console.log("DISCORD: " + message);
+}
+
+function sendError(title, error, channel){
+	console.log("-----"  + "ERROR"+ "------");
+	console.log(error);
+	console.log("----------");
+	channel.sendMessage("**" + title + " Error**\n```" + error.message +"```");
+}
+
+function displayServers(){
+	var guilds = bot.guilds.array();
+	var servers = [];
+
+	for(var i = 0; i < guilds.length; i++){
+		servers.push(guilds[i].name);
+	}
+
+	return "Servers:\n " + servers.join(", ") + "\n";
+}	
+
 bot.on('ready', () => {
 	console.log("AkeBot v" + botVersion);
+	console.log(displayServers());
 
 	setGame(defaultStatus);
 });
 
-bot.on('message', message => {
-	var content = message.content;
+bot.on('disconnect', event =>{
+	console.log("Exited with code: " + event.code);
+	if(event.message)
+		console.log("Message: " + event.message);
 
-	if(isCommand(content, 'exit') && isDev(message)){
+	process.exit(0);
+});
+
+bot.on('guildMemberAdd', guildMember =>{
+	var generalChannel = getChannelByName(guildMember.guild, 'general');
+
+	generalChannel.sendMessage(guildMember.user.username +" Welcome to " + guildMember.guild.name);	
+	botLog(guildMember.guild.name + " welcomes " + guildMember.user.username + " to their server.");
+});
+
+bot.on('message', message => {
+	const msgContent = message.content;
+	const msgChannel = message.channel;
+
+	// Admin commands
+
+	if(isCommand(msgContent, 'exit') && isAdmin(message)){
 		bot.destroy();
 	}
 
-  	if(isCommand(content, 'help')){
+	// GENERAL commands
+
+  	if(isCommand(msgContent, 'help')){
   		message.channel.sendMessage("**In progress!**");
   	}
 
 });
-
-bot.on('disconnect', message =>{
-	process.exit(0);
-});
-
