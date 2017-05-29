c1c262d3744adaee2550f5efc18752ced8fbb68b
Robert <Mesmaroth666@live.com>
2017-05-29 13:44:40 -0700

    Added stats command

--- akebot.js
+++ akebot.js
@@ -878,6 +878,72 @@ bot.on('message', message => {
   		return;
   	}
 
+  	if(isCommand(message.content, 'stats')){
+  		const users = bot.users.array();
+  		const guildMembers = message.guild.members.array();
+  		const channels = bot.channels.array();
+
+  		var guildTotalOnline = 0;
+  		var totalOnline = 0;
+  		var totalTextChannels = 0;
+  		var totalVoiceChannels = 0;
+  		var uptime = botUptime();
+
+  		for(var i = 0; i < guildMembers.length; i++){
+  			if(guildMembers[i].presence.status === 'online'){
+  				guildTotalOnline++;
+  			}
+  		}
+
+  		for(var i = 0; i < users.length; i++){
+  			if(users[i].presence.status === 'online'){
+  				totalOnline++;
+  			}
+  		}
+  		var nonGuildChannels = 0;
+  		for(var i = 0; i < channels.length; i++){
+  			if(channels[i].type === 'text')
+  				totalTextChannels++
+  			else if(channels[i].type === 'voice')
+  				totalVoiceChannels++
+  			else
+  				nonGuildChannels++
+  		}
+
+	  	getInvite(link =>{
+	  		message.channel.send("**Stats**",{  			
+	  			embed: {
+	  				author: {
+				      name: bot.user.username,
+				      url: link,
+				      icon_url: bot.user.displayAvatarURL
+				    },
+	  				color: 1752220,
+	  				fields: [{
+	  					name: "Members",
+	  					value: "`" + bot.users.size + "` Total\n`" + totalOnline + "` Online\n\n`" + message.guild.memberCount + "` this server\n`" + guildTotalOnline + "` online this server",
+	  					inline: true
+	  				}, {
+	  					name: "Channels",
+	  					value: "`" + (bot.channels.size - nonGuildChannels)+ "` Total\n`" + message.guild.channels.size + "` this server\n`" + totalTextChannels + "` Total Text\n`" + totalVoiceChannels + "` Total Voice",
+	  					inline: true
+	  				}, {
+	  					name: "Servers",
+	  					value: bot.guilds.size,
+	  					inline: true
+	  				}, {
+	  					name: "Uptime",
+	  					value: uptime[0] + "d " + uptime[1] + "h " + uptime[2] + "m " + uptime[3] + "s",
+	  					inline: true
+	  				}],
+	  				thumbnail: {
+						url: bot.user.displayAvatarURL
+					}
+	  			}
+	  		});
+	  	});
+  	}
+
   	if(isCommand(message.content, 'source')){
   		mChannel.send("**Source:** https://github.com/Mesmaroth/discord-AkephalosBot").catch(error =>{
   		 	if(error) sentMessageError(error, mChannel);
