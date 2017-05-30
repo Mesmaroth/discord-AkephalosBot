23cdeef35f63ebdb589ebe9de3348cb7a983adcd
Robert <Mesmaroth666@live.com>
2017-05-29 18:06:15 -0700

    Updated login example

--- config/botlogin.js
+++ config/botlogin.js
@@ -1,6 +1,9 @@
-//
+// For token: https://discordapp.com/developers/applications/me
+// For Twitch: https://www.twitch.tv/settings/connections
+// You can get your id by right clicking your name after enabling Developer Mode in Options>Appearance
 
 module.exports = {
-	token: "TOKEN_KEY",
-	twitchClientID: "TWITCH_CLIENT_ID"
+	token: "TOKEN",
+	twitchClientID: "TWITCH_CLIENT_ID",
+	owner_id: "USER_ID"
 }
